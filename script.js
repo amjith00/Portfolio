fbe8da9756930d321a8aeea013a2c06fa431ed52
amjith00 <amjith9900@gmail.com>
2025-02-27 13:39:46 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+ new Swiper('.card-wrapper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrowswkejvjn
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
